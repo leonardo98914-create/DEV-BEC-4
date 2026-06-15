8d30c63e812c292a14e50e9f7558cf4bc236786f
leonardo98914-create <leonardo98914@gmail.com>
2026-06-14 19:30:57 -0600

    Proyecto de Intro a React

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import BusinessCard from "./BusinessCard";
+
+function App() {
+  return (
+    <div>
+      <BusinessCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ BusinessCard.jsx
@@ -0,0 +1,14 @@
+function BusinessCard() {
+  return (
+    <div className="card">
+      <h1>Leonardo González</h1>
+      <h2>Desarrollador Web</h2>
+      <p>
+        Hola, soy estudiante de TI y me gusta desarrollar aplicaciones web con
+        React y JavaScript.
+      </p>
+    </div>
+  );
+}
+
+export default BusinessCard;
